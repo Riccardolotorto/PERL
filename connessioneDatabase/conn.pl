6cbc05313cb371ecf4e547fa03902281b8597065
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-06-06 07:47:46 +0000

    non va connessione

--- connessioneDatabase/conn.pl
+++ connessioneDatabase/conn.pl
@@ -4,7 +4,7 @@ use warnings;
 use DBI;
 
 # Configurazione database
-my $dsn = 'dbi:ODBC:Driver={SQL Server};Server=192.168.40.16;Database=zhao.filippo;';
+my $dsn = 'DBI:mysql:database=zhao.filippo;host=192.168.40.16';
 my $username = 'zhao.filippo'; # Cambia con il tuo username
 my $password = 'xxx123##'; # Cambia con la tua password
 
@@ -17,6 +17,11 @@ print "Connessione avvenuta con successo!\n";
 # Esempio di query
 my $sth = $dbh->prepare("SELECT * FROM Lotorto.Members");
 $sth->execute();
+
+
+while (my @row = $sth->fetchrow_array) {
+    print "ID: $row[0], Username: $row[1], Password: $row[2]\n";
+}
 
 # Chiudere la connessione
 $sth->finish();
